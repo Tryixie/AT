54a874648c532fd01ae50bfa3f055299e19e62bf
Tiffany DURR <102834952+Tryixie@users.noreply.github.com>
2024-10-21 01:22:28 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,119 @@
+export const gamesNew = [
+  {
+    image: `https://www.antretemps.com/upload/image/arctic---cedric-lefebvre----ludonaute-p-image-66253-grande.jpg`,
+    titre: `Arctic`,
+    editeur: `Ludonaute`,
+    auteur: `Cédric Lefebvre`,
+    stock: `En stock`,
+    prix: `17.90 €`,
+    lien: `#`,
+  },
+
+  {
+    image: `https://www.antretemps.com/upload/image/kraftwagen---mathis-cramer---super-meeple-p-image-67050-grande.png`,
+    titre: `Kraftwagen`,
+    editeur: `Super Meeple`,
+    auteur: `Mathis Cramer`,
+    stock: `En stock`,
+    prix: `49.50 €`,
+    lien: `#`,
+  },
+  {
+    image: `https://www.antretemps.com/upload/image/stranger-things---joey-vigour---repos-production-p-image-67047-grande.jpg`,
+    titre: `Stranger Things : Attack of the mind Flayer`,
+    editeur: `Repos Prod`,
+    auteur: `Joey Vigour`,
+    stock: `En stock`,
+    prix: `19.90 €`,
+    lien: `#`,
+  },
+  {
+    image: `https://www.antretemps.com/upload/image/dossiers-criminels---le-secret-des-yakuzas-p-image-67044-grande.jpg`,
+    titre: `Dossiers Criminels : Le secret des Yakuzas`,
+    auteur: ``,
+    editeur: ``,
+    stock: `En stock`,
+    prix: `29.90 €`,
+    lien: `#`,
+  },
+];
+
+export const kids = [
+  {
+    image: `https://www.antretemps.com/upload/image/mimose---sam-et-le-voleur-de-fruits---locomuse-p-image-66552-grande.jpg`,
+    titre: `Mimose & Sam et le Voleur de Fruits`,
+    editeur: `Locomuse`,
+    auteur: ``,
+    stock: `En stock`,
+    prix: `15,90 €`,
+    lien: `#`,
+  },
+
+  {
+    image: `https://www.jeuxdenim.be/images/jeux/GalapaGo_large01.jpg`,
+    titre: `Galapa Go`,
+    editeur: `MJ Games`,
+    auteur: ``,
+    stock: `En stock`,
+    prix: `17.90 €`,
+    lien: `#`,
+  },
+  {
+    image: `https://www.antretemps.com/upload/image/manoir-magique-p-image-66475-grande.jpg`,
+    titre: `Manoir Magique`,
+    editeur: `Multivers`,
+    auteur: `Joey Vigour`,
+    stock: `En stock`,
+    prix: `14.50 €`,
+    lien: `#`,
+  },
+  {
+    image: `https://www.antretemps.com/upload/image/tengu---julien-griffon--xavier-violeau----bankiiiz-editions-p-image-66255-grande.jpg`,
+    titre: `Tengu`,
+    auteur: ``,
+    editeur: `Bankiiz Editions`,
+    stock: `En stock`,
+    prix: `17.90 €`,
+    lien: `#`,
+  },
+];
+
+export const preco = [
+  {
+    image: `https://www.antretemps.com/upload/image/bomb-busters----hisashi-hayashi---cocktail-games-p-image-66207-grande.jpg`,
+    titre: `Bomb Busters`,
+    editeur: `Cocktail Games`,
+    auteur: ``,
+    stock: `oct. 2024`,
+    prix: `30,90 €`,
+    lien: `#`,
+  },
+
+  {
+    image: `https://www.antretemps.com/upload/image/similo-jurassic-world--p-image-66293-grande.jpg`,
+    titre: `Similo : Jurassic World`,
+    editeur: `HG`,
+    auteur: ``,
+    stock: `oct. 2024`,
+    prix: `12.90 €`,
+    lien: `#`,
+  },
+  {
+    image: `https://www.antretemps.com/upload/image/dorf-romantik---le-duel---pegasus-spiele-p-image-66280-grande.jpg`,
+    titre: `Dorf Romantik : Le Duel`,
+    editeur: `Pegasus Spiele`,
+    auteur: ``,
+    stock: `oct. 2024`,
+    prix: `39.90 €`,
+    lien: `#`,
+  },
+  {
+    image: `https://www.antretemps.com/upload/image/raising-robots---l-atelier-des-prodiges----brett-sobol-et-seth-van-orden---lucky-duck-games-p-image-66433-grande.jpg`,
+    titre: `Raising Robots : L'Atelier des Prodiges`,
+    auteur: ``,
+    editeur: `Lucky Duck Games`,
+    stock: `nov. 2024`,
+    prix: `46.90 €`,
+    lien: `#`,
+  },
+];
